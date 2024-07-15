4a4aede9f69715fb40bf4cbc0d5111d24b1b0f0e
Rivadia <rivadia@gmail.com>
2024-07-15 11:31:22 -0600

    docs updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import {defineConfig} from 'vite'
-
+// al agregar el default condig vite, se configura por default la ruta de acceso en la distrubucion
 export default defineConfig({
   base: /blackjack-vite/,
 });
